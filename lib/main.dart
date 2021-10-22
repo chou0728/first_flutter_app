5987b0bfe901036858afdd1e1c465888d0be8231
chou0728 <dean34520@gmail.com>
2021-10-22 21:16:14 +0800

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text('The question!'),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer 1'),
+            ),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer 2'),
+            ),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer 3'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
